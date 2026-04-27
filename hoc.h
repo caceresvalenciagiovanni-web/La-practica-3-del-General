480e1ce5fa9c94a5438c202e49831980dd23a405
caceresvalenciagiovanni-web <caceres.valencia.giovanni@gmail.com>
2026-04-27 14:37:59 -0600

    Define vector structure and update Symbol for vectors
    Added vector structure and updated Symbol entry for vector support.

--- hoc.h
+++ hoc.h
@@ -1,12 +1,32 @@
-typedef struct Symbol { /* entrada de la tabla de s�mbolos */
-	char   *name;
-	short   type;   /* VAR, BLTIN, INDEF */
-	union {
-		double val;	       /* si es VAR */
-		double  (*ptr)(double);      /* s� es BLTIN */
-	} u;
-	struct Symbol   *next;  /* para ligarse al sig. */ 
+/* Se define la estructura Vector para que esté disponible en todo el proyecto */
+struct vector {
+    int n;          /* Cantidad de elementos */
+    double *vec;    /* Arreglo dinámico de componentes */
+};
+typedef struct vector Vector;
+
+/* Se define la entrada de la tabla de símbolos adaptada a vectores */
+typedef struct Symbol {
+    char   *name;   /* Nombre de la variable (ahora puede ser de más de una letra) */
+    short   type;   /* VAR, BLTIN, INDEF */
+    union {
+        Vector *vec;                /* Si es VAR, se almacena un puntero al objeto Vector */
+        Vector *(*ptr)();           /* Si es BLTIN, apunta a una función que retorna un Vector */
+    } u;
+    struct Symbol   *next;          /* Puntero para la lista ligada de la tabla */
 } Symbol;
 
-Symbol *install(char *s,int t, double d), *lookup(char *s);
+Symbol *install(char *s, int t, Vector *v); /* Se instala enviando un puntero Vector */
+Symbol *lookup(char *s);
+
+/* Prototipos de las operaciones vectoriales (migrados desde hoc1) */
+Vector *creaVector(int n); 
+double magnitudVector(Vector *v);
+void imprimeVector(Vector *a); 
+Vector *copiaVector(Vector *a); 
+Vector *sumaVector(Vector *a, Vector *b); 
+Vector *restaVector(Vector *a, Vector *b); 
+Vector *multiVector(Vector *a, Vector *b); 
+Vector *productoPunto(Vector *a, Vector *b);
+Vector *productoCruz(Vector *a, Vector *b);
 
